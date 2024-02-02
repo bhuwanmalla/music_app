15ab03d91c210a383db8b79654aa9bfec1ffb530
Bhuwan Malla <bhuwan.malla54@gmail.com>
2024-02-01 20:46:55 -0500

    initial

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,17 @@
+
+
+
+/**
+ * On install event
+ * 
+ */
+
+// console.log(self);
+// self.addEventListener('install', (event) => {
+//     console.log('SW install!', event);
+// });
+
+
 self.addEventListener('fetch', () => {
 return;
 });
